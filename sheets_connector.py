70930cd4d745e18f55fde48572ca7c4e03346b7d
Alexandre Chelihi-Cazorla <alexandrechelihi@outlook.com>
2025-05-25 14:16:21 +0200

    Update sheets_connector.py

--- sheets_connector.py
+++ sheets_connector.py
@@ -2,17 +2,19 @@ import gspread
 import pandas as pd
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
-import json
 
 def load_data(sheet_name):
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds_dict = json.loads(st.secrets["credentials"])
+
+    creds_dict = st.secrets["credentials"]  # ✅ NE PAS UTILISER json.loads
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
-    sheet = client.open_by_key("1fFjvk0X6vvvM7ZEBfF03yGpNEQhzL1IkM2YrAxJCc70")
+
+    sheet = client.open_by_key("1fFjvk0X6vvvM7ZEBfF03yGpNEQhzL1IkM2YrAxJCc70")  # 👈 Ton ID de Google Sheet
     worksheet = sheet.worksheet(sheet_name)
     data = worksheet.get_all_records()
+
     return pd.DataFrame(data)
