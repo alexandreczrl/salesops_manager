e6c783cd16e9d311cbdb097a6280543e8f173f84
Alexandre Chelihi-Cazorla <alexandrechelihi@outlook.com>
2025-05-25 13:12:58 +0200

    🔧 Corrige lecture credentials depuis les secrets Streamlit

--- sheets_connector.py
+++ sheets_connector.py
@@ -1,13 +1,16 @@
 import gspread
 import pandas as pd
+import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
+import json
 
 def load_data(sheet_name):
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    creds_dict = json.loads(st.secrets["credentials"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
     sheet = client.open_by_key("1fFjvk0X6vvvM7ZEBfF03yGpNEQhzL1IkM2YrAxJCc70")
     worksheet = sheet.worksheet(sheet_name)
